3c0cf7811bacd36dc9193f93456db80c7d773586
frangarcj <frangarcj@gmail.com>
2011-07-18 23:28:23 +0200

    Listado de categorias

--- ejbModule/es/ual/scribene/session/DudaHome.java
+++ ejbModule/es/ual/scribene/session/DudaHome.java
@@ -1,8 +1,12 @@
 package es.ual.scribene.session;
 
 import es.ual.scribene.model.entity.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityHome;
@@ -52,6 +56,59 @@ public class DudaHome extends EntityHome<Duda> {
 	public List<PatronError> getPatronesError() {
 		return getInstance() == null ? null : new ArrayList<PatronError>(
 				getInstance().getPatronesError());
+	}
+
+	public List<String> getCategorias() {
+		return this
+				.getEntityManager()
+				.createQuery(
+						"Select categoria.descripcion from Categoria categoria order by categoria.descripcion")
+				.getResultList();
+	}
+
+	@Override
+	public String persist() {
+
+		try {
+			Categoria categoria = null;
+
+			categoria = (Categoria) this
+					.getEntityManager()
+					.createQuery(
+							"Select categoria from Categoria categoria where descripcion = :descripcion")
+					.setParameter("descripcion",
+							categoriaHome.getInstance().getDescripcion())
+					.getSingleResult();
+
+			getInstance().setCategoria(categoria);
+
+		} catch (Exception e) {
+			getInstance().setCategoria(categoriaHome.getInstance());
+			this.getEntityManager().persist(categoriaHome.getInstance());
+		}
+		return super.persist();
+	}
+
+	@Override
+	public String update() {
+		try {
+			Categoria categoria = null;
+
+			categoria = (Categoria) this
+					.getEntityManager()
+					.createQuery(
+							"Select categoria from Categoria categoria where descripcion = :descripcion")
+					.setParameter("descripcion",
+							categoriaHome.getInstance().getDescripcion())
+					.getSingleResult();
+
+			getInstance().setCategoria(categoria);
+
+		} catch (Exception e) {
+			getInstance().setCategoria(categoriaHome.getInstance());
+			this.getEntityManager().persist(categoriaHome.getInstance());
+		}
+		return super.update();
 	}
 
 }
